32a97ae9b369b61e9339b8a5a9a1bc182b25ec71
rewq0494 <rewq0494@gmail.com>
2023-05-02 16:05:17 +0800

    05201600

--- BradJava/src/tw/org/iii/tutor/Brad72執行序.java
+++ BradJava/src/tw/org/iii/tutor/Brad72執行序.java
@@ -4,23 +4,50 @@ public class Brad72執行序 {
 	public static void main(String[] args) {
 		Brad721 obj1 = new Brad721("a");
 		Brad721 obj2 = new Brad721("b");
-		obj1.start();obj2.start(); // 執行物件方法 start將動作排在CPU後等候執行
-		obj1.run();obj2.run();
-		//run物件執行多次 start只能執行一次（生命只有一次）
+		Brad722 obj3 = new Brad722("c");
+		Thread t1 = new Thread(obj3);
+		obj1.start();
+		obj2.start(); // 執行物件方法 start將動作排在CPU後等候執行
+		t1.start();
+		// run物件執行多次 start只能執行一次（生命只有一次）
 		System.out.println("main");
 	}
 
 }
 
-class Brad721 extends Thread {// 執行序物件
+class Brad721 extends Thread{// 執行序物件
 	String name;
-	Brad721(String name){
+
+	Brad721(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public void run() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(name + "." + i);
+			try {
+				Thread.sleep(100);//讓執行序慢一點
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
+class Brad722 implements Runnable{// 執行序物件
+	String name;
+
+	Brad722(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(name + "." + i);
+			try {
+				Thread.sleep(100);//讓執行序慢一點
+			} catch (InterruptedException e) {
+			}
 		}
 	}
 }
