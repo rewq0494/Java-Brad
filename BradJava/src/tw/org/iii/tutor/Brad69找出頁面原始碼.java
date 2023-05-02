d007635c3102cd7b28bc2ab2cb2f7b7bc76b7aa4
rewq0494 <rewq0494@gmail.com>
2023-05-02 10:42:42 +0800

    05021040

---
+++ BradJava/src/tw/org/iii/tutor/Brad69找出頁面原始碼.java
@@ -0,0 +1,36 @@
+package tw.org.iii.tutor;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Brad69找出頁面原始碼 {
+
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("https://www.ispan.com.tw");
+			HttpURLConnection conn = 
+				(HttpURLConnection)url.openConnection();
+			conn.connect();
+			
+			BufferedReader reader = 
+				new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ( (line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+			reader.close();
+			
+			System.out.println("OK");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
