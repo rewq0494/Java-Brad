44191b80009b3e8b065caa1b563fc54bc15e4458
rewq0494 <rewq0494@gmail.com>
2023-05-02 14:26:56 +0800

    05021425

---
+++ BradJava/src/tw/org/iii/tutor/Brad72執行序.java
@@ -0,0 +1,25 @@
+package tw.org.iii.tutor;
+
+public class Brad72執行序 {
+	public static void main(String[] args) {
+		Brad721 obj1 = new Brad721("a");
+		Brad721 obj2 = new Brad721("b");
+		obj1.start();obj2.start(); // 執行物件方法 start將動作排在CPU後等候執行
+		obj1.run();obj2.run();
+		System.out.println("main");
+	}
+
+}
+
+class Brad721 extends Thread {// 執行序物件
+	String name;
+	Brad721(String name){
+		this.name = name;
+	}
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(name + "." + i);
+		}
+	}
+}
